4a81f00780ba72461e2bb323f24fd4e24676df6b
Zu3zz <zthzththth@163.com>
2019-12-20 22:44:29 +0800

    finish all view with ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,31 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: true
   })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const {userName} = ctx.params;
+  ctx.body = {
+    title: 'this is a profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const {userName, pageIndex} = ctx.params;
+  ctx.body = {
+    title: 'this is a loadMore page',
+    userName,
+    pageIndex
   }
 })
 
