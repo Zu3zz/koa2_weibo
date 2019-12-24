f3bc69dd3fc936b7029d935c16cd3b3d9f333c2b
Zu3zz <zthzththth@163.com>
2019-12-25 00:29:58 +0800

    feat: 开发环境搭建 finished

--- src/app.js
+++ src/app.js
@@ -5,12 +5,25 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+const {isProd} = require('./utils/env')
 
+const {REDIS_CONF} = require('./conf/db')
+
+// 路由注册
 const index = require('./routes')
 const users = require('./routes/users')
+const errorViewRouter = require('./routes/view/error')
 
 // error handler
-onerror(app)
+let errorConf = {}
+if (isProd) {
+  errorConf = {
+    redirect: '/error'
+  }
+}
+onerror(app, errorConf)
 
 // middlewares
 app.use(bodyparser({
@@ -24,17 +37,26 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
-// logger
-// app.use(async (ctx, next) => {
-//   const start = new Date()
-//   await next()
-//   const ms = new Date() - start
-//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-// })
+// session 配置
+app.keys = ['UID_3zz123']
+app.use(session({
+  key: 'weibo.sid', // cookie name 默认是`koa.sid`
+  prefix: 'weibo:sess:', // redis key 的前缀 默认是`koa:sess:`
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000 // ms
+  },
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
+}))
+
 
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
---
+++ test/demo.test.js
@@ -0,0 +1,7 @@
+/**
+ * @description test demo
+ */
+
+ test('test demo', () => {
+   expect(100).toBe(100)
+ })
